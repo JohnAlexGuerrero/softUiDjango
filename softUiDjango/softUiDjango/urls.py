62fe7f005e915b758e236e297a9917a47d02b708
johnalexguerrero <johnalexguerrero@gmail.com>
2023-11-15 23:49:41 -0500

    urls general

--- softUiDjango/softUiDjango/urls.py
+++ softUiDjango/softUiDjango/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('apps.home.urls')),
-    path('auth/', include('apps.authentication.urls')),
+    path('', include('apps.home.urls')),
+    path('accounts/', include('apps.authentication.urls')),
     path('admin/', admin.site.urls),
 ]
